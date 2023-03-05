614137f53b6d771a9fb9275a2c3c01e9e9769edd
szBelial <chc0089@gmail.com>
2023-03-05 15:35:03 +0100

    Añadidas modificaciones de botones e imágenes

--- PetClinic/obj/Debug/PetClinic_Content.g.i.cs
+++ PetClinic/obj/Debug/PetClinic_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/beautiful-dog-women-house.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/principal.png")]
 
 
